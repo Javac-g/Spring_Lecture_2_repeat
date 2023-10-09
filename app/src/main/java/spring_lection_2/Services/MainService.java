6fec730b5dd3cd06ef8dacf57f818fdab4a9b127
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-10-09 18:44:58 +0300

    Implemented .dat file loggin/not log4j

--- app/src/main/java/spring_lection_2/Services/MainService.java
+++ app/src/main/java/spring_lection_2/Services/MainService.java
@@ -8,6 +8,10 @@ import spring_lection_2.Model.Exp;
 import spring_lection_2.Model.Position;
 import spring_lection_2.Model.RequestDTO;
 import spring_lection_2.Model.ResponseDTO;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.util.*;
 
 @Service
@@ -19,6 +23,25 @@ public class MainService {
             return Integer.compare(a.getValue(),b.getValue());
         }
     };
+    public void log(String type,ResponseDTO user){
+        if (type == null || user == null) {
+            throw new IllegalArgumentException();
+        }
+        byte[] data = ("\nType" + type + "\nName: " + user.getFirst_name() + " / " + user.getLast_name()).getBytes();
+        try(FileOutputStream fileOutputStream = new FileOutputStream("log.dat");
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream)){
+
+
+
+                byteArrayOutputStream.write(data);
+                byteArrayOutputStream.writeTo(fileOutputStream);
+                dataOutputStream.writeUTF("\nID: " + user.getId());
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
     public void setPosition(ResponseDTO user,RequestDTO info){
         int max = Collections.max(info.getExpirience(),rule).getValue();
         switch(max){
@@ -41,11 +64,13 @@ public class MainService {
         user.setTool(info.getTool());
         setPosition(user, info);
         database.add(user);
+        log("Created: ", user);
         return user;
     }
     public ResponseDTO find(Integer id){
         for(ResponseDTO x: database){
             if(x.getId().equals(id)){
+                log("Searched: ",x);
                 return x;
             }
         }
@@ -54,11 +79,13 @@ public class MainService {
     public ResponseDTO update(Integer id, RequestDTO info){
         ResponseDTO searchUser = find(id);
         if(searchUser != null){
+            log("Before Update:", searchUser);
             searchUser.setFirst_name(info.getFirst_name());
             searchUser.setLast_name(info.getLast_name());
             searchUser.setId(info.getId());
             searchUser.setTool(info.getTool());
             setPosition(searchUser, info);
+            log("Updated: ", searchUser);
             return searchUser;
         }
         return null;
@@ -68,10 +95,12 @@ public class MainService {
         int index = -1;
         for(int i = 0; i < database.size();i++){
             if(database.get(i).getId().equals(id)){
+                log("Deleted: ", database.get(i));
                 index = i;
             }
         }
         if(index != -1){
+
             return database.remove(index);
             
         }
